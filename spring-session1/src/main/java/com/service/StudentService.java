f6f25deabdc5ea9065b26953cffb617c8787b72f
Nik_Gribok <mushroomicus@gmail.com>
2022-06-09 16:15:52 +0300

    use spring xml & anno

--- spring-session1/src/main/java/com/service/StudentService.java
+++ spring-session1/src/main/java/com/service/StudentService.java
@@ -2,6 +2,7 @@ package com.service;
 
 import com.dao.StudentDAO;
 import com.student.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -10,10 +11,11 @@ public class StudentService {
     public StudentService() {
     }
 
+    @Autowired
     StudentDAO studentDAO;
 //    StudentDAO studentDAO = new StudentDAO();
 
-    public List<Student> getList(){
+    public List<Student> getList() {
         return studentDAO.getStudentsList();
     }
 
